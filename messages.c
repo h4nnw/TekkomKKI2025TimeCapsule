17de64e90ea5d88f1ec9c799e416386b026c489c
han <banna.farhan@ui.ac.id>
2025-11-27 16:24:38 +0700

    saves han message

--- messages.c
+++ messages.c
@@ -2,12 +2,12 @@
 
 // Oscar's message function.
 // Message format: NPM | Full Name | Message
-void oscar() {
-    puts("0006347853 | Oscar Oscar | Just you wait");
+void han() {
+    puts("2506634696 | Banna Farhan Athallah | mungkin ini saatnya selesai mengatai JAWA, mungkin. (all play no foul yeye)\n Also thanks digilab aslab for guiding me through this lab, ima be honest i would be less competent in bapro/fds without them");
 }
 
 int main() {
-    oscar();
+    han();
 
     // Call your function after this.
 
